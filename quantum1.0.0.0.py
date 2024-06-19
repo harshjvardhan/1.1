74a6cf13f345b8f1d7e55eb3dfb80e9d9bed53de
hajava <75774637+harshjvardhan@users.noreply.github.com>
2024-06-19 22:48:20 +0530

    Update quantum1.0.0.0.py
    removed the token no

--- quantum1.0.0.0.py
+++ quantum1.0.0.0.py
@@ -1,7 +1,7 @@
 from qiskit_ibm_runtime import QiskitRuntimeService
  
 # Save an IBM Quantum account and set it as your default account.
-QiskitRuntimeService.save_account(channel="ibm_quantum", token="661f1c49649dd3f73b6280021e15a0f8a841712a4b212947e8b6cff4666a05bd56a1bc984eb34bcbc884e57439187eebb41a0d238bdfe6e918dd3df088f51b5b", set_as_default=True)
+QiskitRuntimeService.save_account(channel="ibm_quantum", token="dofkgkjbiodfgjbdfgbkdmdmcjsfgoibnahjernvkjfgfgbnoskdnvjfsjikrwtb", set_as_default=True)
  
 # Load saved credentials
 service = QiskitRuntimeService()
